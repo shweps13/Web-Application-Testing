fea0a7fb89168f14599908281a7d252b3db2f213
Heorhii Siburov <g.siburov@gmail.com>
2019-09-05 16:19:50 -0700

    some testing

--- web-app-test/src/App.test.js
+++ web-app-test/src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, { strikes } from './App';
 import { render, fireEvent } from '@testing-library/react'
-
+import Dashboard, { strike } from './components/Dashboard'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -14,3 +14,12 @@ test('contains header sign', () => {
   const { getByText, findByText } = render(<App />)
   getByText(/baseball app/i);
 });
+
+test('contains buttons', () => {
+  const { getAllByText } = render(<App />);
+  getAllByText('Strike', /ball/i, /foul/i, /hit/i);
+});
+
+// test('strikes does not be more than 4', () => {
+//   expect(strike(3).not.toBe(5))
+// });
